834d9638376828890da76596bd867853dbc5f51a
Rodrigo Tomé Nieto <tomecafeconrodrigo@live.com>
2019-05-01 09:43:59 +0200

    1.2.1 - Mejoras en los formularios y en el codigo js de electrosa

--- js/electrosa.js
+++ js/electrosa.js
@@ -3,25 +3,12 @@ var contador = 1;
 var buttons = Array.from(document.querySelectorAll('.button-more-details-order'))
 var listDetailsOrder = Array.from(document.querySelectorAll('.more-details-order'))
 
-buttons.forEach((button, number) => button.onclick=function(){
-	if (contador == 1) {
-		buttons[number].classList.remove('icon-line-plus')
-		buttons[number].classList.add('icon-line-minus')
-		listDetailsOrder[number].style.display = "table-row"
-		contador = 0;
-	} else {
-		contador = 1;
-		buttons[number].classList.remove('icon-line-minus')
-		buttons[number].classList.add('icon-line-plus')
-		listDetailsOrder[number].style.display = "none"
-	}
-})
-
-/*buttons.forEach((button, number) => button.onclick=(function(number){
-														return function() {
-															viewDetails(number)
-														}
-													})
+buttons.forEach((button, number) => button.onclick=(
+	function(){
+		return function() {
+			viewDetails(number)
+		}
+	})()
 )
 
 function viewDetails(number) {
@@ -36,4 +23,4 @@ function viewDetails(number) {
 		buttons[number].classList.add('icon-line-plus')
 		listDetailsOrder[number].style.display = "none"
 	}
-}*/
+}
